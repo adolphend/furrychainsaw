735c1bef6a4c37eef468646d62d8fc5239542fe5
adolphend <2015adolphe@gmail.com>
2022-03-19 21:37:42 +0100

    fix(gpu): what the fuck is happening to me

--- tools/toolstensor.py
+++ tools/toolstensor.py
@@ -13,6 +13,6 @@ def heatmapfile(data,  figpath, cmap=None):
 
 def generateFxFy(dimX, dimY, DeltaX, DeltaY):
     y, x = np.indices((dimX, dimY))
-    y, x = torch.from_numpy(y).to(device), torch.from_numpy(x)
+    y, x = torch.from_numpy(y).to(device), torch.from_numpy(x).to(device)
     x, y = (x - int(dimX/2)) * DeltaX, (y - (dimY/2)) * DeltaY
     return x, y
