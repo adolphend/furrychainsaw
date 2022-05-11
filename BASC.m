ef8b477efe14b574d8345b2f4bb61918eb396875
adolphend <71188595+adolphend@users.noreply.github.com>
2022-05-11 15:10:20 +0200

    Update BASC.m
    fix(sensing matrix): "On the Noise-Resilience of OMP with BASC-Based Low Coherence Sensing Matrices"
    Zörlein, Henning, Dejan E. Lazich, and Martin Bossert. "On the noise-resilience of OMP with BASC-based low coherence sensing matrices." Proc. 10th Int. Conf. Sampling Theory Appl.(SampTA). 2013.

--- BASC.m
+++ BASC.m
@@ -6,22 +6,23 @@ function [A, FixedPoint] = BASC(Mu, Nu, gpu)
         vmax = 2^10;
         imax = 1e5;
         epsilon = 0.0000000001;
-        Cs = randi(2, [N, M/2]);
+        Cs = rand([N, M/2]);
         Cs = [Cs, -Cs];
         assert(isequal(size(Cs), [N, M]));
         if gpu
                 Cs = gpuArray(Cs);
         end
+        zero = zeros([Mu, Nu]);
+        if gpu
+                gpuArray(zero);
+        end
         alpha = alphainit;
-        something = 0
+        Cs = Cs ./ vecnorm(Cs);
         while v < vmax
                 FixedPoint = true;
                 i = 0;
                 while i < imax & FixedPoint
-                        Fm = zeros([N, M/2]);
-                        if gpu
-                                gpuArray(Cs);
-                        end
+                        Fm = zero;
                         for m = 1:1:M/2
                                 idx = ones([M,1]);
                                 idx(m) = 0;
@@ -30,23 +31,16 @@ function [A, FixedPoint] = BASC(Mu, Nu, gpu)
                                 sm = Cs(:, m);
                                 sl = Cs(:, idx);
                                 Fm(:, m) = sum(((sm - sl) ./ (vecnorm(sm - sl) .^v))')';
-                                %fm = 0;
-                                %for l = 1:1:M
-                                %       if l ~=m & l ~= m + Nu
-                                %               fm = fm + (Cs(:,m) - Cs(:, l)) / ((norm(Cs(:,m) - Cs(:,l)))^v);
-                                %       end
-                                %end
-                                %Fm(:, m) = fm;
                         end
+                        Fm = Fm ./ vecnorm(Fm);
                         Cs(:, 1:1:M/2) = Cs(:, 1:1:M/2) + alpha * Fm;
-                        Cs(:, M/2+1:1:M) = - Cs(:, 1:1:M/2);[v, alpha, mutual_coherence(Cs(:, 1:1:M/2)', 0)]
+                        Cs(:, 1:1:M/2) = Cs(:, 1:1:M/2) ./ vecnorm(Cs(:, 1:1:M/2));
+                        Cs(:, M/2+1:1:M) = - Cs(:, 1:1:M/2);
                         if sum(vecnorm(Cs(:, 1:1:M/2) - Fm) < epsilon) == M/2
-                                FixedPoint = false;
+                                FixedPoint = false
                         end
-                        i = (i + 1) * 1.2;
+                        i = (i + 1);
                 end
-                v = 2 * v
-                alpha = alphainit / (v - 1)
+                v = 2 * v;
+                alpha = alphainit / (v - 1);
         end
-        A = Cs(:, 1:1:M/2);
-        FixedPoint = ~FixedPoint;
